887ee1ff0fc92f019a35ae52af731bdf67bd6b8c
ilia Naderi <ilianaakooo@gmail.com>
2025-07-02 16:44:15 +0330

    Implemented Report Card Pop-up

---
+++ StudentProfile/main.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("reportModal");
+    const openBtn = document.getElementById("reportCardBtn");
+    const closeBtn = document.getElementById("closeModal");
+
+    openBtn.addEventListener("click", () => {
+      modal.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });

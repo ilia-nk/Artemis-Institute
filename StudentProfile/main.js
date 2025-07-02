8b6a6d6d00f66b75a63a2db5bee0e99eca080ee0
ilia Naderi <ilianaakooo@gmail.com>
2025-07-02 23:08:58 +0330

    Implemented Payment Pop-up

--- StudentProfile/main.js
+++ StudentProfile/main.js
@@ -15,3 +15,21 @@ const modal = document.getElementById("reportModal");
         modal.style.display = "none";
       }
     });
+
+    const paymentModal = document.getElementById("paymentModal");
+    const paymentBtn = document.getElementById("paymentBtn");
+    const closePayment = document.getElementById("closePaymentModal");
+
+    paymentBtn.addEventListener("click", () => {
+      paymentModal.style.display = "flex";
+    });
+
+    closePayment.addEventListener("click", () => {
+      paymentModal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === paymentModal) {
+        paymentModal.style.display = "none";
+      }
+    });
